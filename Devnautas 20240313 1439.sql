327f04f730fc28e3c83b5b4dade4b121ca277049
Átila <157402682+ATILAGATTO@users.noreply.github.com>
2024-03-13 14:41:01 -0300

    Banco de dados

---
+++ Devnautas 20240313 1439.sql
@@ -0,0 +1,139 @@
+-- MySQL Administrator dump 1.4
+--
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.28-MariaDB
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+--
+-- Create schema devnautas
+--
+
+CREATE DATABASE IF NOT EXISTS devnautas;
+USE devnautas;
+
+--
+-- Definition of table `adm`
+--
+
+DROP TABLE IF EXISTS `adm`;
+CREATE TABLE `adm` (
+  `idadm` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL DEFAULT '',
+  `email` varchar(75) NOT NULL DEFAULT '',
+  `senha` varchar(45) NOT NULL DEFAULT '',
+  PRIMARY KEY (`idadm`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `adm`
+--
+
+/*!40000 ALTER TABLE `adm` DISABLE KEYS */;
+/*!40000 ALTER TABLE `adm` ENABLE KEYS */;
+
+
+--
+-- Definition of table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+CREATE TABLE `cliente` (
+  `idcliente` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL DEFAULT '',
+  `idpagamento` int(10) unsigned NOT NULL DEFAULT 0,
+  PRIMARY KEY (`idcliente`,`idpagamento`),
+  KEY `cliente_pagamento` (`idpagamento`),
+  CONSTRAINT `cliente_pagamento` FOREIGN KEY (`idpagamento`) REFERENCES `pagamento` (`idpagamento`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cliente`
+--
+
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+
+
+--
+-- Definition of table `pagamento`
+--
+
+DROP TABLE IF EXISTS `pagamento`;
+CREATE TABLE `pagamento` (
+  `idpagamento` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `dinheiro` varchar(45) NOT NULL DEFAULT '',
+  `cartao` varchar(45) NOT NULL DEFAULT '',
+  PRIMARY KEY (`idpagamento`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pagamento`
+--
+
+/*!40000 ALTER TABLE `pagamento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pagamento` ENABLE KEYS */;
+
+
+--
+-- Definition of table `produto`
+--
+
+DROP TABLE IF EXISTS `produto`;
+CREATE TABLE `produto` (
+  `idproduto` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `idproprietario` int(10) unsigned NOT NULL DEFAULT 0,
+  `nome` varchar(45) NOT NULL DEFAULT '',
+  `foto` blob NOT NULL DEFAULT '',
+  `detalhes` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`idproduto`,`idproprietario`),
+  KEY `produto_proprietario` (`idproprietario`),
+  CONSTRAINT `produto_proprietario` FOREIGN KEY (`idproprietario`) REFERENCES `proprietario` (`idproprietario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `produto`
+--
+
+/*!40000 ALTER TABLE `produto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `produto` ENABLE KEYS */;
+
+
+--
+-- Definition of table `proprietario`
+--
+
+DROP TABLE IF EXISTS `proprietario`;
+CREATE TABLE `proprietario` (
+  `idproprietario` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL DEFAULT '',
+  `foto` blob NOT NULL DEFAULT '',
+  PRIMARY KEY (`idproprietario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `proprietario`
+--
+
+/*!40000 ALTER TABLE `proprietario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `proprietario` ENABLE KEYS */;
+
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
